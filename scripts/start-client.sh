4748b36d0402c19e5d70960c2bb83bb4955a34cd
Yichen <54285184+Dou-bean@users.noreply.github.com>
2021-01-03 16:46:34 -0500

    Rename start-client.sh to startClient.sh

--- scripts/start-client.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd ../target/classes/
-java -classpath .:./../lib/fastjson-1.2.47.jar edu.illinois.web.distgrep.client.ClientUserInterface
---
+++ scripts/startClient.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+cd ../target/classes/
+java -classpath .:./../lib/fastjson-1.2.47.jar edu.illinois.web.distgrep.client.ClientUserInterface
